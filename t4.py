4a214e5c7eaebcf09ea87cb2b6ffc0413910dc98
hhy2022 <huihuay15@gmail.com>
2025-09-19 13:32:52 -0400

    formatting

--- t4.py
+++ t4.py
@@ -1,5 +1,6 @@
 from mesh import generate_mesh
 from dolfinx.log import set_log_level, LogLevel
+
 set_log_level(LogLevel.INFO)
 
 generate_mesh(mesh_size=2e-4)
@@ -20,29 +21,43 @@ import festim as F
 
 import h_transport_materials as htm
 
-#filter nickel and H
-diffusivities_nickel= htm.diffusivities.filter(material="nickel").filter(isotope="h")
+# filter nickel and H
+diffusivities_nickel = htm.diffusivities.filter(material="nickel").filter(isotope="h")
 solubilities_nickel = htm.solubilities.filter(material="nickel").filter(isotope="h")
 
-#material parameters for Nickel
-D_solid= diffusivities_nickel[0].pre_exp.magnitude           #m^2/s
-E_D_solid= diffusivities_nickel[0].act_energy.magnitude      #ev/particle
-K_solid= solubilities_nickel[0].pre_exp.magnitude            #particle m^-3 Pa^-0.5
-E_K_S_solid = solubilities_nickel[0].act_energy.magnitude    #ev/particle
+# material parameters for Nickel
+D_solid = diffusivities_nickel[0].pre_exp.magnitude  # m^2/s
+E_D_solid = diffusivities_nickel[0].act_energy.magnitude  # ev/particle
+K_solid = solubilities_nickel[0].pre_exp.magnitude  # particle m^-3 Pa^-0.5
+E_K_S_solid = solubilities_nickel[0].act_energy.magnitude  # ev/particle
 
-#material parameters for FLiBe
-diffusivities_flibe= htm.diffusivities.filter(material="flibe").filter(isotope="h")
+# material parameters for FLiBe
+diffusivities_flibe = htm.diffusivities.filter(material="flibe").filter(isotope="h")
 solubilities_flibe = htm.solubilities.filter(material="flibe").filter(isotope="h")
 
-D_liquid = diffusivities_flibe[0].pre_exp.magnitude           #m^2/s
-E_D_liquid = diffusivities_flibe[0].act_energy.magnitude      #ev/particle
-K_liquid = solubilities_flibe[0].pre_exp.magnitude            #particle m^-3 Pa^-1
-E_K_S_liquid = solubilities_flibe[0].act_energy.magnitude     #ev/particle. NOTE: This is a negative value?
+D_liquid = diffusivities_flibe[0].pre_exp.magnitude  # m^2/s
+E_D_liquid = diffusivities_flibe[0].act_energy.magnitude  # ev/particle
+K_liquid = solubilities_flibe[0].pre_exp.magnitude  # particle m^-3 Pa^-1
+E_K_S_liquid = solubilities_flibe[
+    0
+].act_energy.magnitude  # ev/particle. NOTE: This is a negative value?
 
 # Define materials
 
-mat_solid  = F.Material(D_0=D_solid,  E_D=E_D_solid, K_S_0=K_solid,  E_K_S=E_K_S_solid, solubility_law="sievert")
-mat_liquid = F.Material(D_0=D_liquid, E_D=E_D_liquid, K_S_0=K_liquid, E_K_S=E_K_S_liquid, solubility_law="henry")
+mat_solid = F.Material(
+    D_0=D_solid,
+    E_D=E_D_solid,
+    K_S_0=K_solid,
+    E_K_S=E_K_S_solid,
+    solubility_law="sievert",
+)
+mat_liquid = F.Material(
+    D_0=D_liquid,
+    E_D=E_D_liquid,
+    K_S_0=K_liquid,
+    E_K_S=E_K_S_liquid,
+    solubility_law="henry",
+)
 
 fluid_volume = F.VolumeSubdomain(id=1, material=mat_liquid)
 solid_volume = F.VolumeSubdomain(id=2, material=mat_solid)
@@ -69,14 +84,27 @@ my_model.mesh = F.Mesh(mesh, coordinate_system="cylindrical")
 my_model.facet_meshtags = facet_tags
 my_model.volume_meshtags = cell_tags
 
-my_model.subdomains = [solid_volume, fluid_volume, out_surf, 
-                       left_bc_liquid,left_bc_top_Ni, left_bc_middle_Ni,left_bc_bottom_Ni,
-                       top_Ni_bottom, Ds_Ni_left, Up_Ni_left,
-                       Liquid_top, mem_Ni_bottom, bottom_Ni_top,
-                       liquid_solid_interface]
+my_model.subdomains = [
+    solid_volume,
+    fluid_volume,
+    out_surf,
+    left_bc_liquid,
+    left_bc_top_Ni,
+    left_bc_middle_Ni,
+    left_bc_bottom_Ni,
+    top_Ni_bottom,
+    Ds_Ni_left,
+    Up_Ni_left,
+    Liquid_top,
+    mem_Ni_bottom,
+    bottom_Ni_top,
+    liquid_solid_interface,
+]
 
 my_model.method_interface = "penalty"
-interface = F.Interface(id=99, subdomains=[solid_volume, fluid_volume], penalty_term=1e28)
+interface = F.Interface(
+    id=99, subdomains=[solid_volume, fluid_volume], penalty_term=1e28
+)
 
 my_model.interfaces = [interface]
 
@@ -99,92 +127,45 @@ my_model.species = [H]
 
 my_model.temperature = 773
 
-upstream_volume_surfaces = [
-    mem_Ni_bottom, bottom_Ni_top, Up_Ni_left
-]
+upstream_volume_surfaces = [mem_Ni_bottom, bottom_Ni_top, Up_Ni_left]
 
-downstream_volume_surfaces = [
-    top_Ni_bottom, Ds_Ni_left, Liquid_top
-]
+downstream_volume_surfaces = [top_Ni_bottom, Ds_Ni_left, Liquid_top]
 
-out_surface_bc = F.FixedConcentrationBC(
-    subdomain=out_surf, species=H, value=0.0
-)
+out_surface_bc = F.FixedConcentrationBC(subdomain=out_surf, species=H, value=0.0)
 
 P_up = 1e5  # Pa
 
-my_model.boundary_conditions = [
-   F.SievertsBC(subdomain=s, species=H, pressure=P_up, S_0=K_solid, E_S=E_K_S_solid)   ###NOTE: E_s can not be 0.
-    for s in upstream_volume_surfaces
-] + [out_surface_bc] + [
-    F.FixedConcentrationBC(subdomain=s, species=H, value=0.0)
-    for s in downstream_volume_surfaces]
+my_model.boundary_conditions = (
+    [
+        F.SievertsBC(
+            subdomain=s, species=H, pressure=P_up, S_0=K_solid, E_S=E_K_S_solid
+        )  ###NOTE: E_s can not be 0.
+        for s in upstream_volume_surfaces
+    ]
+    + [out_surface_bc]
+    + [
+        F.FixedConcentrationBC(subdomain=s, species=H, value=0.0)
+        for s in downstream_volume_surfaces
+    ]
+)
 
 my_model.settings = F.Settings(atol=1e-10, rtol=1e-10, transient=False)
 
 
+from cylindrical_flux import CylindricalFlux
 
-
-# import ufl
-# import numpy as np
-# import dolfinx
-# from mpi4py import MPI
-# # here we define our own SurfaceFlux class that takes into account the cylindrical coordinate system
-
-
-# class CylindricalFlux(F.SurfaceFlux):
-#     azimuth_range: tuple[float, float] = (0.0, 2 * np.pi)
-
-#     def compute(
-#         self,
-#         u: dolfinx.fem.Function | ufl.indexed.Indexed,
-#         ds: ufl.Measure,
-#         entity_maps=None,
-#     ):
-#         """Computes the value of the flux at the surface
-
-#         J =int(- D * grad(c) . n * r dS)
-
-#         Args:
-#             u: field for which the flux is computed
-#             ds: surface measure of the model
-#             entity_maps: entity maps relating parent mesh and submesh
-#         """
-#         from scifem import assemble_scalar
-
-#         # obtain mesh normal from field
-#         # if case multispecies, solution is an index, use sub_function_space
-#         if isinstance(u, ufl.indexed.Indexed):
-#             mesh = self.field.sub_function_space.mesh
-#         else:
-#             mesh = u.function_space.mesh
-#         n = ufl.FacetNormal(mesh)
-#         x = ufl.SpatialCoordinate(mesh)
-#         r = x[0]
-
-#         flux = assemble_scalar(
-#             dolfinx.fem.form(
-#                 -self.D * r * ufl.dot(ufl.grad(u), n) * ds(self.surface.id),
-#                 entity_maps=entity_maps,
-#             )
-#         )
-
-#         flux *= self.azimuth_range[1] - self.azimuth_range[0]
-
-#         self.value = flux
-#         self.data.append(self.value)
-
-
-
-# flux_in = CylindricalFlux(field=H, surface=upstream_volume_surfaces)
-# flux_out_1 = CylindricalFlux(field=H, surface=downstream_volume_surfaces)
+flux_in = CylindricalFlux(field=H, surface=upstream_volume_surfaces)
+downstream_fluxes = [CylindricalFlux(field=H, surface=downstream_volume_surfaces)]
 # flux_out_2 = CylindricalFlux(field=H, surface=out_surf)
-# flux_out = F.CombinedFlux([flux_out_1, flux_out_2])
 
 my_model.exports = [
-    F.VTXSpeciesExport(field=H, filename="out-species_solid.bp", subdomain=solid_volume),
-    F.VTXSpeciesExport(field=H, filename="out-species_fluid.bp", subdomain=fluid_volume),
-]#+ [flux_in, flux_out]
+    F.VTXSpeciesExport(
+        field=H, filename="out-species_solid.bp", subdomain=solid_volume
+    ),
+    F.VTXSpeciesExport(
+        field=H, filename="out-species_fluid.bp", subdomain=fluid_volume
+    ),
+]  # + [flux_in, flux_out]
 
 my_model.initialise()
 my_model.run()
@@ -231,10 +212,10 @@ my_model.run()
 c_flibe = 1.32e25
 c_ni = 2.17e25
 
-K_s_ni = solubilities_nickel[0].value(my_model.temperature)   # particle m^-3 Pa^-0.5
+K_s_ni = solubilities_nickel[0].value(my_model.temperature)  # particle m^-3 Pa^-0.5
 K_s_flibe = solubilities_flibe[0].value(my_model.temperature)  # particle m^-3 Pa^-1
 
-expected_c_flibe = (c_ni / K_s_ni)**2 * K_s_flibe 
+expected_c_flibe = (c_ni / K_s_ni) ** 2 * K_s_flibe
 
 print("Expected concentration in FLiBe (particle/m^3): ", expected_c_flibe)
 print("Real concentration in FLiBe (particle/m^3): ", c_flibe)
