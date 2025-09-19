88e26ff2c20c0a8abefec2947f70584275eeb2b2
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2025-09-19 12:55:00 -0400

    added initial guess

--- t4.py
+++ t4.py
@@ -19,8 +19,12 @@ import festim as F
 D_solid, D_liquid = 2.0, 2.0
 K_solid, K_liquid = 3.0, 6.0
 E_K_S_solid, E_K_S_liquid = 0.0, 0.0
-mat_solid  = F.Material(D_0=D_solid,  E_D=0.0, K_S_0=K_solid,  E_K_S=E_K_S_solid, solubility_law="sievert")
-mat_liquid = F.Material(D_0=D_liquid, E_D=0.0, K_S_0=K_liquid, E_K_S=E_K_S_liquid, solubility_law="henry")
+mat_solid = F.Material(
+    D_0=D_solid, E_D=0.0, K_S_0=K_solid, E_K_S=E_K_S_solid, solubility_law="sievert"
+)
+mat_liquid = F.Material(
+    D_0=D_liquid, E_D=0.0, K_S_0=K_liquid, E_K_S=E_K_S_liquid, solubility_law="henry"
+)
 
 fluid_volume = F.VolumeSubdomain(id=1, material=mat_liquid)
 solid_volume = F.VolumeSubdomain(id=2, material=mat_solid)
@@ -36,7 +40,6 @@ Up_Ni_left = F.SurfaceSubdomain(id=7)
 Liquid_top = F.SurfaceSubdomain(id=8)
 mem_Ni_bottom = F.SurfaceSubdomain(id=9)
 bottom_Ni_top = F.SurfaceSubdomain(id=10)
-liquid_solid_interface = F.SurfaceSubdomain(id=99)
 
 my_model = F.HydrogenTransportProblemDiscontinuous()
 
@@ -46,14 +49,26 @@ my_model.mesh = F.Mesh(mesh, coordinate_system="cylindrical")
 my_model.facet_meshtags = facet_tags
 my_model.volume_meshtags = cell_tags
 
-my_model.subdomains = [solid_volume, fluid_volume, out_surf, 
-                       left_bc_liquid,left_bc_top_Ni, left_bc_middle_Ni,left_bc_bottom_Ni,
-                       top_Ni_bottom, Ds_Ni_left, Up_Ni_left,
-                       Liquid_top, mem_Ni_bottom, bottom_Ni_top,
-                       liquid_solid_interface]
+my_model.subdomains = [
+    solid_volume,
+    fluid_volume,
+    out_surf,
+    left_bc_liquid,
+    left_bc_top_Ni,
+    left_bc_middle_Ni,
+    left_bc_bottom_Ni,
+    top_Ni_bottom,
+    Ds_Ni_left,
+    Up_Ni_left,
+    Liquid_top,
+    mem_Ni_bottom,
+    bottom_Ni_top,
+]
 
 my_model.method_interface = "penalty"
-interface = F.Interface(id=99, subdomains=[solid_volume, fluid_volume], penalty_term=1e6)
+interface = F.Interface(
+    id=99, subdomains=[solid_volume, fluid_volume], penalty_term=250
+)
 
 my_model.interfaces = [interface]
 
@@ -76,37 +91,50 @@ my_model.species = [H]
 
 my_model.temperature = 773
 
-upstream_volume_surfaces = [
-    mem_Ni_bottom, bottom_Ni_top, Up_Ni_left
-]
+upstream_volume_surfaces = [mem_Ni_bottom, bottom_Ni_top, Up_Ni_left]
 
-downstream_volume_surfaces = [
-    top_Ni_bottom, Ds_Ni_left, Liquid_top
-]
+downstream_volume_surfaces = [top_Ni_bottom, Ds_Ni_left, Liquid_top]
 
-out_surface_bc = F.FixedConcentrationBC(
-    subdomain=out_surf, species=H, value=0.0
-)
+out_surface_bc = F.FixedConcentrationBC(subdomain=out_surf, species=H, value=0.0)
 
 P_up = 1e5  # Pa
 
-my_model.boundary_conditions = [
-   F.SievertsBC(subdomain=s, species=H, pressure=P_up, S_0=K_solid, E_S=0.000001)   ###NOTE: E_s can not be 0?
-    for s in upstream_volume_surfaces
-] + [out_surface_bc] + [
-    F.FixedConcentrationBC(subdomain=s, species=H, value=0.0)
-    for s in downstream_volume_surfaces]
+my_model.boundary_conditions = (
+    [
+        F.SievertsBC(
+            subdomain=s, species=H, pressure=P_up, S_0=K_solid, E_S=0.000001
+        )  ###NOTE: E_s can not be 0?
+        for s in upstream_volume_surfaces
+    ]
+    + [out_surface_bc]
+    + [
+        F.FixedConcentrationBC(subdomain=s, species=H, value=0)
+        for s in downstream_volume_surfaces
+    ]
+)
 
 my_model.settings = F.Settings(atol=1e-10, rtol=1e-10, transient=False)
 
 my_model.exports = [
-    F.VTXSpeciesExport(field=H, filename="out-species_solid.bp", subdomain=solid_volume),
-    F.VTXSpeciesExport(field=H, filename="out-species_fluid.bp", subdomain=fluid_volume),
+    F.VTXSpeciesExport(
+        field=H, filename="out-species_solid.bp", subdomain=solid_volume
+    ),
+    F.VTXSpeciesExport(
+        field=H, filename="out-species_fluid.bp", subdomain=fluid_volume
+    ),
 ]
 
-my_model.initialise()
-my_model.run()
+from dolfinx.log import set_log_level, LogLevel
 
+set_log_level(LogLevel.INFO)
+
+my_model.initialise()
+
+# add a non-zero initial guess for the solution
+for domain in my_model.volume_subdomains:
+    domain.u.sub(0).x.array[:] = 0.1
+
+my_model.run()
 
 
 # # from dolfinx.io import XDMFFile
